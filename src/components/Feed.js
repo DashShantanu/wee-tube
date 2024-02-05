17acfa9999dd00833e517ccbf68a1297048d2224
Shantanu <papun260801@gmail.com>
2024-02-05 23:45:35 +0530

    added sidebar with category buttons

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,10 +1,60 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+
+import { Sidebar, Videos } from './';
 
 const Feed = () => {
     return (
-        <div>
+        <Stack
+            sx={{
+                flexDirection: { sx: 'column', md: 'row' }
+            }}
+        >
+            <Box
+                sx={{
+                    height: { sx: 'auto', md: '92vh' },
+                    borderRight: '1px solid #3d3d3d',
+                    px: { sx: 0, md: 2 }
+                }}
+            >
+                <Sidebar />
 
-        </div>
+                <Typography
+                    className='copyright'
+                    variant="body2"
+                    sx={{
+                        mt: 1.5,
+                        color: '#fff'
+                    }}
+                >
+                    Copyright 2024 Shantanu Dash
+                </Typography>
+            </Box>
+
+            <Box
+                p={2}
+                sx={{
+                    overflowY: 'auto',
+                    height: '90vh',
+                    flex: 2
+                }}
+            >
+                <Typography
+                    variant='h4'
+                    fontWeight='bold'
+                    mb={2}
+                    sx={{ color: '#fff' }}
+                >
+                    New <span style={{ color: '#f31503' }} >
+                        videos
+                    </span>
+                </Typography>
+
+                <Videos
+                    videos={[]}
+                />
+            </Box>
+        </Stack>
     )
 };
 
