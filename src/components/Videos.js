2b123bbc58456fedce6c886f37322571d80bdcaa
Shantanu <papun260801@gmail.com>
2024-02-07 02:14:56 +0530

    added video,channel cards on feed with image & info

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import { Stack, Box } from '@mui/material';
+
+import { VideoCard, ChannelCard } from './'
 
 const Videos = ({ videos }) => {
     // console.log(videos);
 
     return (
-        <div>
-
-        </div>
+        <Stack
+            direction='row'
+            flexWrap='wrap'
+            justifyContent='start'
+            gap={2}
+        >
+            {
+                videos.map((item, idx) => (
+                    <Box
+                        key={idx}
+                    >
+                        {/* Render either a video card or channel detail card according to each data in list */}
+                        {
+                            item.id.videoId &&
+                            <VideoCard video={item} />
+                        }
+                        {
+                            // item.id.channelId &&
+                            // <ChannelCard channelDetail={item} />
+                        }
+                    </Box>
+                ))
+            }
+        </Stack>
     )
 };
 
